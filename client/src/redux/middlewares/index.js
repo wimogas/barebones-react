cede4cf9a6f7419b69101c6307e2108045a81988
Guillem Moya <gmoya@amg-dev.fr>
2020-05-22 09:00:00 +0200

    reverse logger

--- client/src/redux/middlewares/index.js
+++ client/src/redux/middlewares/index.js
@@ -1,9 +1,7 @@
 export const logger = ({getState}) => next => action => {
-        if(process.env.NODE_ENV !== 'production') {
-                console.log('----------------------------------')
-                console.log('%cprev state', 'color:gray; font-weight: bold', getState())
-                console.log('%caction', 'color:turquoise; font-weight: bold', action)
-                next(action)
-                console.log('%cnext state', 'color:limegreen; font-weight: bold', getState())
-        }
+        console.log('----------------------------------')
+        console.log('%cprev state', 'color:gray; font-weight: bold', getState())
+        console.log('%caction', 'color:turquoise; font-weight: bold', action)
+        next(action)
+        console.log('%cnext state', 'color:limegreen; font-weight: bold', getState())
 }
