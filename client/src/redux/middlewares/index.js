c310757a4a4d943f7079635ed104205e46067444
Guillem Moya <gmoya@amg-dev.fr>
2020-05-22 08:55:48 +0200

    remove logger from production

--- client/src/redux/middlewares/index.js
+++ client/src/redux/middlewares/index.js
@@ -1,7 +1,9 @@
 export const logger = ({getState}) => next => action => {
-        console.log('----------------------------------')
-        console.log('%cprev state', 'color:gray; font-weight: bold', getState())
-        console.log('%caction', 'color:turquoise; font-weight: bold', action)
-        next(action)
-        console.log('%cnext state', 'color:limegreen; font-weight: bold', getState())
+        if(process.env.NODE_ENV !== 'production') {
+                console.log('----------------------------------')
+                console.log('%cprev state', 'color:gray; font-weight: bold', getState())
+                console.log('%caction', 'color:turquoise; font-weight: bold', action)
+                next(action)
+                console.log('%cnext state', 'color:limegreen; font-weight: bold', getState())
+        }
 }
